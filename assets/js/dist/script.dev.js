f7c2c8a6a301c9c4ab1d42bc4f295737cc71c7db
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-01-23 21:04:57 -0600

    completed beer button function
    -completed the beer button function
    -linked punkapiv2 in our site
    -when i click the button
    -then random beer + info appears

--- assets/js/dist/script.dev.js
+++ assets/js/dist/script.dev.js
@@ -1,19 +1,29 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('https://api.punkapi.com/v2/beers/random').then(function (response) {
-    return response.json();
-  }).then(function (data) {
-    console.log(data);
-    var name = data[0].name;
-    console.log(name);
-    var description = data[0].description;
-    console.log(description);
-    var volume = data[0].volume;
-    var volumeValue = volume.value;
-    var volumeUnit = volume.unit;
-    console.log(volumeUnit);
-    console.log(volumeValue);
-  });
+  var startBtn = document.querySelector('.beer-button');
+  var randomBeer = document.querySelector('.random-beer');
+  var descriptionDisplay = document.querySelector('.description');
+
+  function getData() {
+    fetch('https://api.punkapi.com/v2/beers/random').then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+      var name = data[0].name;
+      console.log(name);
+      var description = data[0].description;
+      console.log(description);
+      var volume = data[0].volume;
+      var volumeValue = volume.value;
+      var volumeUnit = volume.unit;
+      console.log(volumeUnit);
+      console.log(volumeValue);
+      randomBeer.innerHTML = name + ' ' + volumeValue + volumeUnit;
+      descriptionDisplay.innerHTML = description;
+    });
+  }
+
+  startBtn.addEventListener('click', getData);
 });
 //# sourceMappingURL=script.dev.js.map
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+const startBtn = document.querySelector('.beer-button')
+const randomBeer= document.querySelector('.random-beer')
+const descriptionDisplay = document.querySelector('.description')
+
+function getData() {
     fetch('https://api.punkapi.com/v2/beers/random')
     .then(response => {
         return response.json()
@@ -14,7 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const volumeUnit = volume.unit
     console.log(volumeUnit)
     console.log(volumeValue)
-    
+
+    randomBeer.innerHTML = name + ' ' + volumeValue + volumeUnit 
+    descriptionDisplay.innerHTML = description
+
     })
+
+}
+
+startBtn.addEventListener('click', getData)
+ 
 
 })
