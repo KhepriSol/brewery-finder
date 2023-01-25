17ef1feeb5c894c3a0af92f82d8320e4f41605f2
El Elnimeiry <elsadekelnimeriy@gmail.com>
2023-01-24 20:00:33 -0600

    test

---
+++ brewery-finder-main/README.md
@@ -0,0 +1,29 @@
+# brewery-finder
+
+## Description
+
+[Link to deployed application]()
+
+## Installation
+
+N/A
+
+## Usage
+
+![alt text](./assets/images/Screenshot1.png)
+![alt text](./assets/images/Screenshot2.png)
+
+## Credits
+
+El Elnimeiry
+Jane Collazo
+Jonah Kane
+Sara Schroeder
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+# general notes - jonah
+
+<!-- <script src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBRMdBEtvNSdYwH1s7dN5GRVKo7_UPIoEw&callback=initMap" defer></script> -->
---
+++ brewery-finder-main/assets/js/dist/script.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//random beer genorator api
+document.addEventListener('DOMContentLoaded', function () {
+  var startBtn = document.querySelector('.beer-button');
+  var randomBeer = document.querySelector('.random-beer');
+  var descriptionDisplay = document.querySelector('.description');
+
+  function getData() {
+    fetch('https://api.punkapi.com/v2/beers/random').then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+      var name = data[0].name;
+      console.log(name);
+      var description = data[0].description;
+      console.log(description);
+      var volume = data[0].volume;
+      var volumeValue = volume.value;
+      var volumeUnit = volume.unit;
+      console.log(volumeUnit);
+      console.log(volumeValue);
+      randomBeer.innerHTML = name + ' ' + volumeValue + volumeUnit;
+      descriptionDisplay.innerHTML = description;
+    });
+  }
+
+  startBtn.addEventListener('click', getData);
+});
+//# sourceMappingURL=script.dev.js.map
